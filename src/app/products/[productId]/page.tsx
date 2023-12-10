1b2efa9b7e8d15817614dd6d5bc59ec569884499
Astar <bahouidi99@gmail.com>
2023-12-10 20:25:35 +0100

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,27 @@
-export default function Product({ params }: { params: { productId: string } }) {
+import { Metadata } from "next";
+
+type ProductProps = { params: { productId: string } };
+
+// export const generateMetadata = ({ params }: ProductProps): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`,
+//   };
+// };
+
+export const generateMetadata = async ({
+  params,
+}: ProductProps): Promise<Metadata> => {
+  const productName = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Samsung ${params.productId}`);
+    }, 200);
+  });
+
+  return {
+    title: `Product ${productName}`,
+  };
+};
+
+export default function Product({ params }: ProductProps) {
   return <h1>Details about product {params.productId}</h1>;
 }
