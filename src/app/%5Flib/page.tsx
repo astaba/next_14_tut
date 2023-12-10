f81857efc8d19a72473b8a2de0824aeadb96cfaa
Astar <bahouidi99@gmail.com>
2023-12-10 13:15:31 +0100

    Private route

---
+++ src/app/%5Flib/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PrivateRoute() {
+  return (
+    <>
+      <h1>Private Route</h1>
+      <h4>With underscrore encoded to %5F</h4>
+    </>
+  );
+}
+
+export default PrivateRoute;
---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PrivateRoute() {
+  return <h1>Private Route</h1>;
+}
+
+export default PrivateRoute;
