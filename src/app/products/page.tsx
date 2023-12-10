5b85a2159c8516ab114f0f08c236dfc3e50f3781
Astar <bahouidi99@gmail.com>
2023-12-11 00:06:00 +0100

    Link component navigation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,16 +1,27 @@
+import Link from "next/link";
+
 export default function Products() {
   return (
     <>
       <h1>Products Page</h1>
+      <Link href={"/"}>Home</Link>
       <ul>
         <li>
-          <h2>Product 1</h2>
+          <h2>
+            <Link href={"/products/1"}>Product 1</Link>
+          </h2>
         </li>
         <li>
-          <h2>Product 2</h2>
+          <h2>
+            <Link href={"/products/2"}>Product 2</Link>
+          </h2>
         </li>
         <li>
-          <h2>Product 3</h2>
+          <h2>
+            <Link href={"/products/3"} replace>
+              Product 3
+            </Link>
+          </h2>
         </li>
       </ul>
     </>
